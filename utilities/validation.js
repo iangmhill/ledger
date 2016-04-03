bdb9a0ea6dad9d15e7c4063d249701daab2a689d
Ian Hill <ian.hill@students.olin.edu>
2016-04-03 19:26:09 -0400

    Document server side javascript

--- utilities/validation.js
+++ utilities/validation.js
@@ -1,8 +1,26 @@
+/**
+ * Model field validation module.
+ * @module utilities/validation
+ * @requires NPM:q
+ * @requires NPM:validator
+ * @requires models/userModel
+ */
+ 
+'use strict';
+
 var validator = require('validator');
 var User      = require('../models/userModel');
 var q         = require('q');
 
-var validation = {
+module.exports = {
+  /**
+   * Validates the four important properties of a user.
+   * @param {string} username The username to be validated.
+   * @param {string} password The password to be validated.
+   * @param {string} name The real name to be validated.
+   * @param {string} email The email address to be validated.
+   * @return {Promise.<boolean>} Whether all four properties are valid.
+   */
   user: function(username, password, name, email) {
     var deferred = q.defer();
     var validation = this;
@@ -15,6 +33,11 @@ var validation = {
     });
     return deferred.promise;
   },
+  /**
+   * Validates a username.
+   * @param {string} username The username to be validated.
+   * @return {Promise.<boolean>} Whether the username is valid.
+   */
   username: function(username) {
     var deferred = q.defer();
     User.find({username: username}, function(err, users) {
@@ -22,15 +45,28 @@ var validation = {
     })
     return deferred.promise;
   },
+  /**
+   * Validates a password.
+   * @param {string} password The password to be validated.
+   * @return {boolean} Whether the password is valid.
+   */
   password: function(password) {
     return typeof password === 'string';
   },
+  /**
+   * Validates a real name.
+   * @param {string} username The real name to be validated.
+   * @return {boolean} Whether the real name is valid.
+   */
   name: function(name) {
     return typeof name === 'string' && name.trim().length > 0;
   },
+  /**
+   * Validates an email.
+   * @param {string} username The email to be validated.
+   * @return {boolean} Whether the email is valid.
+   */
   email: function(email) {
     return typeof email === 'string' && validator.isEmail(email);
   }
 };
-
-module.exports = validation;
